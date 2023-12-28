8a083c6b2c408216a3f38032ea317506e680e4ef
Cheryl TD <139712656+MyLittleToy@users.noreply.github.com>
2023-12-28 15:06:30 +0000

    Update 3.Restaurantdb_Analysing customer behaviour.sql

--- 3.Restaurantdb_Analysing customer behaviour.sql
+++ 3.Restaurantdb_Analysing customer behaviour.sql
@@ -66,11 +66,3 @@ FROM
 where order_id in ('440', '2075', '1957', '330', '2675')
 group by order_id, category;
 
--- Recommended Analysis
--- What were the least and most ordered items? What categories were they in?
-
--- What do the highest spend orders look like? Which items did they buy and how much did they spend?
-
--- Were there certain times that had more or less orders?
-
--- Which cuisines should we focus on developing more menu items for based on the data?
